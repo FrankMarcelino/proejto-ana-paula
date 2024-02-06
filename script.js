d5cb3d2b127d7c124de54213a682e7113b1169c1
frankMarcelino <frank.silva.ti@gmail.com>
2024-02-06 15:17:03 -0300

    programei as abas

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+// === esquiva não ===
+function sim() {
+    // alert("Hoje tem! ;)");
+    // redireciona para um URL após clicar no SIM
+    location.href = "https://www.youtube.com/watch?v=btPJPFnesV4";
+}
+
+function desvia(btn) {
+    // btn declarado na função
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log('opa, desviei...');
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
